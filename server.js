21706542b055702364b61cc6cdc8d6002c1f190a
abhinav soni <abhinavsoni316@gmail.com>
2026-06-23 15:30:21 +0530

    configured post method to send data to queries.txt file

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 import http from 'http';
+import fs from 'fs'
+import { File } from './file.js';
+
+const file = new File();
+
 
 
 export function serverfn()
 {
 const server = http.createServer((req,res)=>{
-    res.end('welcome')
+
+    if(req.method=='POST')
+    {
+        let body = ''
+        req.on('data',(chunks)=>{
+            body +=chunks;
+        })
+           
+
+      req.on('end',()=>{
+        file.appendFile(body)
+        res.end('data sent')
+      })
+
+    }
+    else
+    {
+        res.end('get req')
+    }
 })
 
 server.listen(5000,()=>console.log('server began'))
 
 }
 
+serverfn()
