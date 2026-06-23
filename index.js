c0c70f996723effe4f95e29c77eff9293492afda
abhinav soni <abhinavsoni316@gmail.com>
2026-06-23 21:20:32 +0530

    sent response when email sent event emited

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import { Servercl } from "./server.js";
-
+import { Mailevent } from "./event.js";
+import { event } from "./mail.js";
 
 const server = new Servercl()
 server.serverfn()
+
+
+//const event = new Mailevent();
+event.on('emailSent',()=>{
+    console.log('email sent successfully')
+})
+
+//event.mailsent()
