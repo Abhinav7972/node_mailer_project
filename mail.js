168a3d6fcebec506292b201a55282d94b289e73f
abhinav soni <abhinavsoni316@gmail.com>
2026-06-23 17:20:19 +0530

    fix data fetching bugs from mail.js

--- mail.js
+++ mail.js
@@ -2,14 +2,16 @@ import  dotenv from 'dotenv'
 dotenv.config({ path: './pass.env' });
 import nodemailer from 'nodemailer'
 import { File } from './file.js';
+import { json } from 'stream/consumers';
 
 const file = new File();
-const fdata = file.readFile()
 export class Emailcl
 {
  async mailer() {
     const email = process.env.EMAIL_USER?.trim();
     const pass = process.env.EMAIL_PASS?.trim();
+const fdata =  JSON.parse(file.readFile())
+    
     if (!email || !pass) {
         console.error('Error: EMAIL_USER or EMAIL_PASS is missing from .env');
         return;
@@ -25,10 +27,10 @@ export class Emailcl
 
 try {
     const mail = await tr.sendMail({
-        from: `abhinav <${email}>`,
-        to: 'soniabhinav1996@gmail.com',
+        from: `${fdata.from} <${email}>`,
+        to: fdata.to,
         subject: 'test',
-        text: fdata
+        text: fdata.message
     });
     console.log('mail sent');
     console.log(file.readFile())
